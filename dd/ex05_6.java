3586181b93cc05417e32cd0269f07f97efc0de7a
wnsdud4761 <62161544+wnsdud4761@users.noreply.github.com>
2020-10-03 15:47:36 +0900

    Add files via upload

---
+++ dd/ex05_6.java
@@ -0,0 +1,27 @@
+package dd;
+
+class Weapon{
+	protected int fire() {
+		return 1;
+	}
+}
+
+class Cannon extends Weapon{
+	@Override
+	protected int fire() {
+		return 10;
+	}
+}
+
+public class ex05_6 {
+
+	public static void main(String[] args) {
+		Weapon weapon;
+		weapon = new Weapon();
+		System.out.println("기본 무기의 살상 능력은"+weapon.fire());
+		weapon = new Cannon();
+		System.out.println("대포의 살상 능력은"+weapon.fire());
+
+	}
+
+}
---
+++ dd/ex05_7.java
@@ -0,0 +1,34 @@
+package dd;
+
+abstract class Calculator{
+	public abstract int add(int a, int b);
+	public abstract int subtract(int a, int b);
+	public abstract double average(int[] a);
+}
+
+public class ex05_7 extends Calculator {
+	@Override
+	public int add(int a, int b) {
+		return a+b;
+	}
+	@Override
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	@Override
+	public double average(int[] a) {
+		double sum = 0;
+		for(int i=0; i<a.length; i++)
+			sum += a[i];
+		return sum/a.length;
+	}
+	
+	public static void main(String[] args) {
+		ex05_7 c = new ex05_7();
+		System.out.println(c.add(2, 3));
+		System.out.println(c.subtract(2, 3));
+		System.out.println(c.average(new int [] {2,3,4}));
+
+	}
+
+}
